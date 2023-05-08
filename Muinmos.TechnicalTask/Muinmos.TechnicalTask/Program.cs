735fad739bcf440f9724e17db03735808124f355
Mojtaba Yekta <mojtaba.a63@gmail.com>
2023-05-08 15:59:43 +0200

    MLogger class created

--- Muinmos.TechnicalTask/Muinmos.TechnicalTask/Program.cs
+++ Muinmos.TechnicalTask/Muinmos.TechnicalTask/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Muinmos.TechnicalTask;
+
+var mLogger = new MLogger();
+
+Task.Run(() => { for (int i = 0; i < 2000; i++) mLogger.Log($"Caller 1: Log message {i}"); });
+Task.Run(() => { for (int i = 2000; i < 4000; i++) mLogger.Log($"Caller 2: Log message {i}"); });
+Task.Run(() => { for (int i = 4000; i < 6000; i++) mLogger.Log($"Caller 3: Log message {i}"); });
+Task.Run(() => { for (int i = 6000; i < 8000; i++) mLogger.Log($"Caller 4: Log message {i}"); });
+Task.Run(() => { for (int i = 8000; i < 10000; i++) mLogger.Log($"Caller 5: Log message {i}"); });
+Task.Run(() => { for (int i = 0; i < 500; i++) mLogger.Log($"Caller 6: Log extra message {i}"); });
+
+Task.Delay(10000).Wait(); // Just for Test: wait to make sure all threads completed.
+
+mLogger.Flush();
